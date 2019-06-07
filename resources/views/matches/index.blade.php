fd056d7c75055a52c0a03c98d15ef18091d6fc53
arianjoan <arianproven@gmail.com>
2019-06-07 17:31:45 -0300

    Match view

--- resources/views/matches/index.blade.php
+++ resources/views/matches/index.blade.php
@@ -1,25 +1,88 @@
 @extends('index')
 @section('indexSection')
-<table class="table">
-    <thead>
-      <tr>
-        <th scope="col">Id</th>
-        <th scope="col">Fecha</th>
-        <th scope="col">Equipo</th>
-        <th scope="col"></th>
-        <th scope="col">Equipo</th>
-      </tr>
-    </thead>
-    <tbody>
-    @foreach($matches as $match)
-      <tr>
-        <th scope="row">{{$match->id}}</th>
-        <td>{{$match->dateMatch}}</td>
-        <td>{{$match->teamA->nameTeam}}</td>
-        <td>VS</td>
-        <td>{{$match->teamB->nameTeam}}</td>
-      </tr>
-    @endforeach
-    </tbody>
-  </table>
-@endsection()
+
+    <h1>GRUPO A</h1>
+
+    <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">Fecha</th>
+            <th scope="col">Equipo</th>
+            <th scope="col"></th>
+            <th scope="col">Equipo</th>
+          </tr>
+        </thead>
+        <tbody>
+        @foreach($matches as $match)
+            @if($match->name == "A")
+                <tr>
+                    <th scope="row">{{$match->id}}</th>
+                    <td>{{date("d/m/y",strtotime($match->dateMatch))}}</td>
+                    <td>{{$match->teamA->nameTeam}}</td>
+                    <td>VS</td>
+                    <td>{{$match->teamB->nameTeam}}</td>
+                </tr>
+                @endif
+        @endforeach
+        </tbody>
+      </table>
+
+
+    <h1>GRUPO B</h1>
+
+    <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">Id</th>
+            <th scope="col">Fecha</th>
+            <th scope="col">Equipo</th>
+            <th scope="col"></th>
+            <th scope="col">Equipo</th>
+          </tr>
+        </thead>
+        <tbody>
+        @foreach($matches as $match)
+            @if($match->name == "B")
+                <tr>
+                    <th scope="row">{{$match->id}}</th>
+                    <td>{{date("d/m/y",strtotime($match->dateMatch))}}</td>
+                    <td>{{$match->teamA->nameTeam}}</td>
+                    <td>VS</td>
+                    <td>{{$match->teamB->nameTeam}}</td>
+                </tr>
+                @endif
+        @endforeach
+        </tbody>
+      </table>
+
+
+
+    <h1>GRUPO C</h1>
+
+    <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">Id</th>
+            <th scope="col">Fecha</th>
+            <th scope="col">Equipo</th>
+            <th scope="col"></th>
+            <th scope="col">Equipo</th>
+          </tr>
+        </thead>
+        <tbody>
+        @foreach($matches as $match)
+            @if($match->name == "C")
+                <tr>
+                    <th scope="row">{{$match->id}}</th>
+                    <td>{{date("d/m/y",strtotime($match->dateMatch))}}</td>
+                    <td>{{$match->teamA->nameTeam}}</td>
+                    <td>VS</td>
+                    <td>{{$match->teamB->nameTeam}}</td>
+                </tr>
+                @endif
+        @endforeach
+        </tbody>
+      </table>
+
+
+@endSection()
