cc9ae4f12b5ec8d98541374dc2183dce35065e34
ezemacchi <eze_macchimallo@hotmail.com>
2019-06-08 11:03:26 -0300

    updated migrations

--- database/migrations/2019_06_08_041637_create_stats_table.php
+++ database/migrations/2019_06_08_041637_create_stats_table.php
@@ -15,7 +15,18 @@ class CreateStatsTable extends Migration
     {
         Schema::create('stats', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->bigInteger('id_teamA');
+            $table->bigInteger('id_teamB');
+            $table->bigInteger('id_result');
+            $table->unsignedInteger('scoreA');
+            $table->unsignedInteger('scoreB');
             $table->timestamps();
+        });
+
+        Schema::table('stats', function(Blueprint $table) {
+            $table->foreign('id_teamA')->references('id')->on('teams');
+            $table->foreign('id_teamB')->references('id')->on('teams');
+            $table->foreign('id_result')->references('id')->on('results');
         });
     }
 
